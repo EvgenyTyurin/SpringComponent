ba0029af1ad3f9c8a0200cfa0fc432434b1af185
EvgenyTyurin <beshere@yandex.ru>
2019-07-25 16:00:50 +0300

    initial

---
+++ src/main/java/evgenyt/springdemo/TestBean.java
@@ -0,0 +1,17 @@
+package evgenyt.springdemo;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
